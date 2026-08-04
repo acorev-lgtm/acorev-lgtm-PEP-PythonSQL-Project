79c8a17f580557da58b33f155dbe26a39c921856
acorev-lgtm <acorev-lgtm@github.com>
2026-08-04 06:43:48 +0000

    Test run: 0/4 passed, 0 failed, 0 skipped - 2026-08-04T06:43:48.415Z

--- src/main/main.py
+++ src/main/main.py
@@ -88,21 +88,21 @@ def load_and_clean_call_logs(file_path):
 # example: 1,105.0,4 - where 1 is the userId, 105.0 is the avgDuration, and 4 is the numCalls.
 def write_user_analytics(csv_file_path):
     with open(csv_file_path, mode="w", encoding="utf-8") as f:
-        writer = csv.write(f)
+        writer = csv.writer(f)
         header = writer.writerows(["userId","avgDuration","numCalls"])
         duration_dict = {}
         calls_dict = {}
-    #     try:
-    #         cursor.execute("""SELECT userId, startTime, endTime
-    #                             FROM callLogs
-    #                             GROUP BY userId
-    #                             ORDER BY userId ASC;""")
+        try:
+            cursor.execute("""SELECT userId, startTime, endTime
+                                FROM callLogs
+                                GROUP BY userId
+                                ORDER BY userId ASC;""")
     #         time_rows = cursor.fetchall()
     #         print(time_rows)
-    #     except Exception as e:
-    #         print(e)
-    #         continue
-    # print("write_user_analytics")
+        except Exception as e:
+            print(e)
+            continue
+    print("write_user_analytics")
 
 
 # This function will write the callLogs ordered by userId, then start time.
